c06a39657d6891b1a4e2bbd4c506ec5d513e0581
Diego Liascovich <padie78@gmail.com>
2026-04-07 19:19:13 +0300

    fix code

--- terraform/modules/app_orchestrator/main.tf
+++ terraform/modules/app_orchestrator/main.tf
@@ -61,20 +61,34 @@ resource "aws_appsync_datasource" "analytics_lambda_ds" {
   }
 }
 
-# 6. Resolver: Mapeo de la Query al DataSource
-resource "aws_appsync_resolver" "analytics_query_resolver" {
+# 6. Resolvers dinámicos para todas las Queries del SMS
+resource "aws_appsync_resolver" "analytics_resolvers" {
+  for_each = toset([
+    "getYearlyKPI",
+    "getMonthlyKPI",
+    "getEvolution",
+    "getIntensity",
+    "getForecast",
+    "getAuditReport",
+    "searchInvoices"
+  ])
+
   api_id      = aws_appsync_graphql_api.app_orchestrator.id
   type        = "Query"
-  field       = "getOrganizationAnalytics"
+  field       = each.key
   data_source = aws_appsync_datasource.analytics_lambda_ds.name
 
+  # Template optimizado para pasar todo el contexto (incluyendo identity y info)
   request_template = <<EOF
 {
     "version": "2017-02-28",
     "operation": "Invoke",
     "payload": {
         "arguments": $util.toJson($context.arguments),
-        "identity": $util.toJson($context.identity)
+        "identity": $util.toJson($context.identity),
+        "info": {
+            "fieldName": "$context.info.fieldName"
+        }
     }
 }
 EOF
