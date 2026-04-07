15cf141287c90b0ff22bbb7da67b61c59b63853e
Diego Liascovich <padie78@gmail.com>
2026-04-07 19:58:51 +0300

    fix codefix code

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -76,7 +76,6 @@ resource "aws_lambda_function" "analytics" {
   # Este nombre coincide exactamente con lo que pide el outputs.tf
   function_name = "${var.project_name}-analytics-${var.environment}"
   filename      = data.archive_file.analytics_zip.output_path
-  source_code_hash = filebase64sha256("${path.module}/../analytics_lambda.zip")
   handler       = "src/index.handler"
   runtime       = "nodejs20.x"
   role          = var.lambda_role_arn
@@ -94,6 +93,8 @@ resource "aws_lambda_function" "analytics" {
       DYNAMO_TABLE = var.dynamo_table_name
     }
   }
+
+  source_code_hash = data.archive_file.analytics_zip.output_base64sha256
 }
 
 # ==============================================================================
