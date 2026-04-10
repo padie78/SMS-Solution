c7ce5c6eea37be19653fb19cbe3edd0262819512
Diego Liascovich <padie78@gmail.com>
2026-04-10 17:00:54 +0300

    fix

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "upload_bucket_name" { value = module.storage.bucket_id }
 output "dynamo_table_name"  { value = module.database.table_name }
-output "api_endpoint"       { value = module.compute.appsync_url }
+output "api_endpoint"       { value = module.api.appsync_url }
