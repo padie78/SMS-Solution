599c5fbc4af7033dc53246ee5ef6d5524710ad47
Diego Liascovich <padie78@gmail.com>
2026-04-10 10:49:58 +0300

    fix code

--- terraform/resolvers/getYearlyKPI.js
+++ terraform/resolvers/getYearlyKPI.js
@@ -1,14 +1,15 @@
 import { util } from '@aws-appsync/utils';
 
 export function request(ctx) {
-    const { year } = ctx.arguments;
-    const orgId = ctx.identity.claims['custom:orgId']; // Seguridad: solo sus datos
+  return {
+    operation: 'Invoke',
+    payload: {
+      year: ctx.arguments.year,
+      method: 'getYearlyKPI'
+    },
+  };
+}
 
-    return {
-        operation: 'GetItem',
-        key: util.dynamodb.toMapValues({
-            PK: `ORG#${orgId}`,
-            SK: `STATS#YEAR#${year}`
-        }),
-    };
+export function response(ctx) {
+  return ctx.result;
 }
