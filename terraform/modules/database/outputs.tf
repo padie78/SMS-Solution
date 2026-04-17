48e5a48329a4a5ced44edab5bb043849bd097655
Diego Liascovich <padie78@gmail.com>
2026-04-17 16:14:15 +0300

    fix

--- terraform/modules/database/outputs.tf
+++ terraform/modules/database/outputs.tf
@@ -7,3 +7,8 @@ output "table_arn" {
   description = "ARN de la tabla DynamoDB"
   value       = aws_dynamodb_table.emissions_table.arn
 }
+
+output "table_stream_arn" {
+  # Asegúrate de que el nombre coincida con el recurso de tu tabla (ej: emissions_table)
+  value = aws_dynamodb_table.emissions_table.stream_arn
+}
