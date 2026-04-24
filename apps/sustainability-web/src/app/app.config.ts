c813e828f09932755887804d047d6499f17890ac
Diego Liascovich <padie78@gmail.com>
2026-04-24 21:51:53 +0300

    fix

--- apps/sustainability-web/src/app/app.config.ts
+++ apps/sustainability-web/src/app/app.config.ts
@@ -1,27 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import { MessageService } from 'primeng/api'; // <--- IMPORTANTE
-import { Amplify } from 'aws-amplify';
 
-// Configuración de AWS
-// Tip: Más adelante podremos inyectar estos valores desde un environment.ts
-Amplify.configure({
-  API: {
-    GraphQL: {
-      endpoint: 'https://75nymxzbp5dddnsnehigmroili.appsync-api.eu-central-1.amazonaws.com/graphql',
-      region: 'eu-central-1',
-      defaultAuthMode: 'userPool'
-    }
-  }
-});
+// Importes de PrimeNG necesarios para el inyector
+import { MessageService, PrimeNGConfig } from 'primeng/api'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    MessageService
+    MessageService, // Para los Toasts,
+    provideHttpClient(), // Para las llamadas HTTP
+    PrimeNGConfig   // <--- AGREGA ESTO. Es vital para componentes complejos de PrimeNG
   ]
 };
