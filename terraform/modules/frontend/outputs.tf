52617928ecfb259a7b9563bf699b6c5e48e9a002
Diego Liascovich <padie78@gmail.com>
2026-04-24 13:38:45 +0300

    fix

--- terraform/modules/frontend/outputs.tf
+++ terraform/modules/frontend/outputs.tf
@@ -6,3 +6,7 @@ output "cloudfront_domain" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.webapp_bucket.id
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.cdn.id
+}
