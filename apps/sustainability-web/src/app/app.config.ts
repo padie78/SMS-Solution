f5f0b01ac4e90c972e4204af9b9d8d88c06b091b
Diego Liascovich <padie78@gmail.com>
2026-04-24 21:47:03 +0300

    fix

--- apps/sustainability-web/src/app/app.config.ts
+++ apps/sustainability-web/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { MessageService } from 'primeng/api'; // <--- IMPORTANTE
 import { Amplify } from 'aws-amplify';
 
 // Configuración de AWS
@@ -20,6 +21,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimations() // Mantiene las animaciones de PrimeNG estables
+    provideAnimations(),
+    MessageService
   ]
 };
