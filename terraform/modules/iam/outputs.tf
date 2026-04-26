34797f22a475b3e5adee96224cd3bb05c1ea73b6
Diego Liascovich <padie78@gmail.com>
2026-04-26 18:36:31 +0300

    fix

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -21,3 +21,19 @@ output "api_lambda_role_arn" {
 output "lambda_role_arn" {
   value = aws_iam_role.lambda_role.arn
 }
+
+# modules/iam/outputs.tf
+
+output "lambda_role_name" {
+  description = "Nombre del rol genérico para Lambdas"
+  value       = aws_iam_role.lambda_role.name
+}
+
+# Aprovechá y confirmá que tenés estos otros por si la API los pide:
+output "api_lambda_role_name" {
+  value = aws_iam_role.api_lambda_role.name
+}
+
+output "worker_lambda_role_name" {
+  value = aws_iam_role.worker_lambda_role.name
+}
