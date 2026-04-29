4bc541c0ff59aa64e7626d215926780983fb9dd9
Diego Liascovich <padie78@gmail.com>
2026-04-29 19:07:06 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -186,3 +186,9 @@ moved {
   from = aws_s3_bucket_policy.allow_cloudfront
   to   = module.frontend_storage.aws_s3_bucket_policy.allow_cloudfront
 }
+
+# Al final de tu main.tf raíz
+moved {
+  from = module.frontend.aws_s3_bucket.webapp_bucket
+  to   = module.frontend_storage.aws_s3_bucket.webapp_bucket
+}
