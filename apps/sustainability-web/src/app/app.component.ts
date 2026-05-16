0febbbfd43d16a6772bbd06f94dcb7f8f65f2075
Diego Liascovich <padie78@gmail.com>
2026-05-16 17:55:09 +0300

    f

--- apps/sustainability-web/src/app/app.component.ts
+++ apps/sustainability-web/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MainAppLayoutComponent } from './ui/templates/main-app-layout/main-app-layout.component';
-/* test test test tessttttt t ttt*/
+/* test test test tessttfttt t ttt*/
 @Component({
   selector: 'app-root',
   standalone: true,
